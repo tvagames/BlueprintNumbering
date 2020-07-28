9728397b9509388acef77809ee187bb2890a7609
tvagames <tvagames@gmail.com>
2020-07-28 19:11:19 +0900

    init

---
+++ BluepringNumbering/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace BluepringNumbering
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDir = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.numMajor = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numMiner = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkEda = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.btnPreview = new System.Windows.Forms.Button();
+            this.btnApply = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numMajor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMiner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtDir
+            // 
+            this.txtDir.Location = new System.Drawing.Point(57, 30);
+            this.txtDir.Name = "txtDir";
+            this.txtDir.Size = new System.Drawing.Size(387, 19);
+            this.txtDir.TabIndex = 0;
+            this.txtDir.TextChanged += new System.EventHandler(this.txtDir_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(450, 27);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(33, 25);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // numMajor
+            // 
+            this.numMajor.Location = new System.Drawing.Point(75, 65);
+            this.numMajor.Name = "numMajor";
+            this.numMajor.Size = new System.Drawing.Size(46, 19);
+            this.numMajor.TabIndex = 2;
+            this.numMajor.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "フォルダ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "数字桁数";
+            // 
+            // numMiner
+            // 
+            this.numMiner.Location = new System.Drawing.Point(365, 65);
+            this.numMiner.Name = "numMiner";
+            this.numMiner.Size = new System.Drawing.Size(46, 19);
+            this.numMiner.TabIndex = 2;
+            this.numMiner.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numMiner.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(306, 67);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "枝番桁数";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // chkEda
+            // 
+            this.chkEda.AutoSize = true;
+            this.chkEda.Location = new System.Drawing.Point(153, 66);
+            this.chkEda.Name = "chkEda";
+            this.chkEda.Size = new System.Drawing.Size(136, 16);
+            this.chkEda.TabIndex = 4;
+            this.chkEda.Text = "枝番があれば0埋めする";
+            this.chkEda.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(27, 114);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(281, 328);
+            this.listBox1.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(356, 114);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(281, 328);
+            this.listBox2.TabIndex = 5;
+            // 
+            // btnPreview
+            // 
+            this.btnPreview.Location = new System.Drawing.Point(450, 61);
+            this.btnPreview.Name = "btnPreview";
+            this.btnPreview.Size = new System.Drawing.Size(77, 25);
+            this.btnPreview.TabIndex = 1;
+            this.btnPreview.Text = "プレビュー";
+            this.btnPreview.UseVisualStyleBackColor = true;
+            this.btnPreview.Click += new System.EventHandler(this.btnPreview_Click);
+            // 
+            // btnApply
+            // 
+            this.btnApply.Location = new System.Drawing.Point(560, 448);
+            this.btnApply.Name = "btnApply";
+            this.btnApply.Size = new System.Drawing.Size(77, 25);
+            this.btnApply.TabIndex = 1;
+            this.btnApply.Text = "適用";
+            this.btnApply.UseVisualStyleBackColor = true;
+            this.btnApply.Click += new System.EventHandler(this.btnApply_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 501);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.chkEda);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numMiner);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numMajor);
+            this.Controls.Add(this.btnApply);
+            this.Controls.Add(this.btnPreview);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.txtDir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numMajor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMiner)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDir;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.NumericUpDown numMajor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numMiner;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkEda;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button btnPreview;
+        private System.Windows.Forms.Button btnApply;
+    }
+}
+
---
+++ BluepringNumbering/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BluepringNumbering
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (this.folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                this.txtDir.Text = this.folderBrowserDialog1.SelectedPath;
+            }
+        }
+
+        private void txtDir_TextChanged(object sender, EventArgs e)
+        {
+            ViewList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPreview_Click(object sender, EventArgs e)
+        {
+            ViewList();
+
+        }
+
+        private void btnApply_Click(object sender, EventArgs e)
+        {
+            if (!System.IO.Directory.Exists(this.txtDir.Text))
+            {
+                return;
+            }
+            var dir = new System.IO.DirectoryInfo(this.txtDir.Text);
+            foreach (var item in renameList)
+            {
+                var path1 = System.IO.Path.Combine(dir.FullName, item.Key);
+                var path2 = System.IO.Path.Combine(dir.FullName, item.Value);
+                System.IO.File.Move(path1, path2);
+            }
+            MessageBox.Show("completed");
+        }
+
+        private Dictionary<string, string> renameList = new Dictionary<string, string>();
+
+        private void ViewList()
+        {
+            if (!System.IO.Directory.Exists(this.txtDir.Text))
+            {
+                return;
+            }
+            this.listBox1.Items.Clear();
+            this.listBox2.Items.Clear();
+            this.renameList.Clear();
+
+            var dir = new System.IO.DirectoryInfo(this.txtDir.Text);
+            var files = dir.GetFiles();
+            var nums = new char[]{ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+            foreach (var file in files)
+            {
+                var phase = 0;
+                var prefix = "";
+                var suffix = "";
+                var major = "";
+                var miner = "";
+                var delimiter = "";
+                foreach (var c in file.Name)
+                {
+                    if (nums.Contains(c))
+                    {
+                        if (phase <= 1)
+                        {
+                            phase = 1;
+                            major += c;
+                        }
+                        else if (phase <= 3)
+                        {
+                            phase = 3;
+                            miner += c;
+                        }
+                    }
+                    else
+                    {
+                        if (phase == 0)
+                        {
+                            prefix += c;
+                        }
+                        else if (phase <= 2)
+                        {
+                            delimiter += c;
+                            phase = 2;
+                        }
+                        else if (phase <= 4)
+                        {
+                            suffix += c;
+                            phase = 4;
+                        }
+                    }
+                }
+
+                if (major.Length < numMajor.Value)
+                {
+                    major = major.PadLeft((int)numMajor.Value, '0');
+                }
+                var m = 0;
+                if (chkEda.Checked && !string.IsNullOrWhiteSpace(miner) && int.TryParse(miner, out m) && miner.Length < numMiner.Value)
+                {
+                    miner = miner.PadLeft((int)numMiner.Value, '0');
+                }
+
+                var newFileName = prefix + major + delimiter + miner + suffix;
+                this.listBox1.Items.Add(file.Name);
+                this.listBox2.Items.Add(newFileName);
+                renameList.Add(file.Name, newFileName);
+            }
+        }
+    }
+}
